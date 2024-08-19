868cf1d3f41778771afc23e907a20a299417bec3
Gulistanistar <164290112+Gulistanistar@users.noreply.github.com>
2024-08-19 11:56:10 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=SJtAGJID#mf_Vt6uYTVUai9-vPy4Z4k2TO6FFLLQCUQrjeEOdMtY' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=DVEHkbLR#Eq-SizUDg4IV9RF7ICzf5fsFBLTSfheuI5kFOps9W0E' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    

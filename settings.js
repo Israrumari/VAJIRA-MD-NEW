e0bd69dfa2ba7c44f61e7c970c01f04e6c1a2fc5
Israrumari <israrumari0312@gmail.com>
2024-08-23 17:22:10 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '923308363433' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=DVEHkbLR#Eq-SizUDg4IV9RF7ICzf5fsFBLTSfheuI5kFOps9W0E' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=zBdGlAhA#Q7hVIfaztUSpMld8l3Yy8h6PW4V_pg7pYl_t4BQ2l5A' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
